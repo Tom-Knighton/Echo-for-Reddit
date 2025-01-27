3800fe3becb732ac15b08997d6be42e8e13fd563
Tom K <tomknighton@icloud.com>
2025-01-27 21:39:43 +0000

    Initial commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Tom Knighton on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
